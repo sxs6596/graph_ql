d170ce48563ce8cd1a6a1f4346145a02428c0434
sxs6596 <sxs6596@mavs.uta.edu>
2024-08-12 14:41:33 -0500

    second commit

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const app = express();
 
 const PORT = 8000; 
 
-
+app.length('/api/users',(req,res)=>{
+    return res.send('Hello World');
+})
 app.listen(PORT,()=>console.log(`server listening on the port ${PORT}`));
